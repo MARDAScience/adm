0afd295809fbebe95b7529571c992daeb5787585
dbuscombe-usgs <dbuscombe@gmail.com>
2021-03-11 11:40:56 -0800

    3/11/21

---
+++ download_test_images.py
@@ -0,0 +1,40 @@
+# Written by Dr Daniel Buscombe, Marda Science LLC
+# for the USGS Coastal Change Hazards Program
+#
+# MIT License
+#
+# Copyright (c) 2020, Marda Science LLC
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import tensorflow as tf
+
+urls = [
+"https://github.com/dbuscombe-usgs/aom/releases/download/0.0.1/20181006_VA_to_Oregon_Inlet_1m_DSM_adjExt_cog.tif",
+"https://github.com/dbuscombe-usgs/aom/releases/download/0.0.1/20181006_VA_to_Oregon_Inlet_RGBAvg_1m_NODATA_NAD83_2011_UTM18_cog.tif",
+"https://github.com/dbuscombe-usgs/aom/releases/download/0.0.1/20181007_Hattaras_Inlet_to_Ocracoke_Inlet_1m_DSM_adjExt_cog.tif",
+"https://github.com/dbuscombe-usgs/aom/releases/download/0.0.1/20181007_Hattaras_Inlet_to_Ocracoke_Inlet_RGBAvg_1m_NODATA_NAD83_2011_UTM18_cog.tif"
+]
+
+files = [u.split('/')[-1] for u in urls]
+
+for file, url in zip(files,urls):
+    filename = os.path.join(os.getcwd(), file)
+    tf.keras.utils.get_file(filename, url)
